f75e424319ec75ce468f7f969b059dfb39ebf2c6
carpediem <1090990640@qq.com>
2021-12-02 00:07:37 +0800

    12/02

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -2,7 +2,7 @@
   <div class="product">
     <ul class="product-fullpage-indicator">
       <li v-for="(item, index) in products" :key="item.id">
-        <img :src="item.logo" alt="" />
+        <img :src="item.logo" alt />
       </li>
     </ul>
   </div>
@@ -25,8 +25,18 @@ export default {
 
 <style lang="less" scoped>
 .product {
+  position: fixed;
+  top: 60px;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  min-width: 700px;
+  min-height: 500px;
   &-fullpage-indicator {
-   
+    position: absolute;
+    right: 50px;
+    top: 50%;
+    transform: translateY(-50%);
   }
 }
 </style>
