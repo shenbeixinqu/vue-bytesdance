cc4a1674c60ff313ce326a93f14a131f63ae5bf3
dingshuai <1090990640@qq.com>
2021-12-02 16:21:09 +0800

    12/02

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="product">
     <ul class="product-fullpage-indicator">
-      <li v-for="(item, index) in products" :key="item.id">
+      <li
+        v-for="(item, index) in products"
+        :key="item.id"
+        class="product-fullpage-indicator-item"
+        @click="activeIndex = index"
+      >
         <img :src="item.logo" alt />
       </li>
     </ul>
+    <transition :name="transitionName">
+      <div>
+        <div class="content">
+          <div class="logo">
+            
+          </div>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -13,10 +27,13 @@ export default {
   data() {
     return {
       products: [],
+      activeIndex: 0,
+      transitionName: "",
     };
   },
   created() {
     this.request.get("/products").then((response) => {
+      console.log("res", response)
       this.products = response;
     });
   },
@@ -37,6 +54,20 @@ export default {
     right: 50px;
     top: 50%;
     transform: translateY(-50%);
+    &-item {
+      margin: 22px 0;
+      width: 5vh;
+      height: 5vh;
+      min-height: 30px;
+      min-width: 30px;
+      cursor: pointer;
+      border-radius: 50%;
+      // overflow: hidden;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
   }
 }
 </style>
